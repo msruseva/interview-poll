a4d20c4c4bce92a418af5a5bb4ce079db888d9d6
msruseva <msruseva@gmail.com>
2022-01-11 17:16:29 +0200

    Add schema with tables for polls, questions and answers

---
+++ db/migrate/20220109173859_create_answers.rb
@@ -0,0 +1,9 @@
+class CreateAnswers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :answers do |t|
+      t.integer :question_id
+      t.string :answer_text
+      t.timestamps
+    end
+  end
+end

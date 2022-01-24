356ed5499d77bb64a3ae55c7bddc9e7e4d4bd52d
msruseva <msruseva@gmail.com>
2022-01-24 08:36:32 +0200

    Add changes in the asset files and helper javascript actions
    The helper actions in the application.js file makes it easier to add new
    answers and question to a poll, so as notification after voting for a
    specific question.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,43 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require jquery
+//= require jquery_ujs
+//= require jquery.validate
 import "@hotwired/turbo-rails"
 import "controllers"
+
+$(document).on ('click', '.add_questions', function(event) {
+  var time, regexp;
+  time = new Date().getTime();
+  regexp = new RegExp($(this).data('id'), 'g');
+
+  $('.question_fields').append($(this).data('fields').replace(regexp, time));
+  return event.preventDefault();
+});
+
+$(document).on ('click', '.add_answers', function(event) {
+  var time, regexp;
+  time = new Date().getTime();
+  regexp = new RegExp($(this).data('id'), 'g');
+
+  $('.answer_fields').append($(this).data('fields').replace(regexp, time));
+  return event.preventDefault();
+});
+
+$(document).ready(function() {
+  $('#question_form').validate({
+    focusInvalid: false,
+    errorClass: 'alert alert-warning',
+    errorElement: "div",
+    errorPlacement: function(error, element) { $('#question_form').before(error); },
+    rules: {
+      'question_form[id]': {
+        required: true
+      }
+    },
+    messages: {
+      'question_form[id]': {
+        required: "Please select one of the questions."
+      }
+    }
+  });
+});
